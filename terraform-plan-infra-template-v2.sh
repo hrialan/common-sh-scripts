fe0ec093f01fcb9a52552211a2e1fc7d823fd93c
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2024-09-24 11:19:25 +0200

    Fix

--- terraform-plan-infra-template-v2.sh
+++ terraform-plan-infra-template-v2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd iac/deploy-steps/1.infra/
-terraform init -backend-config=../vars/backend/dv.tfvars
+terraform init -backend-config=../vars/backend/dv.tfvars -upgrade
 terraform plan -var-file=../vars/tfvars/dv.tfvars
